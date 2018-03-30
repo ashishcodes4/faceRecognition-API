5ac1482015035ac16164b39850f71b12a8ffacb6
Ashish Singh <ashishcodes4@gmail.com>
2018-03-30 17:57:32 +0530

    initial commit for the server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = exxpress();
+const bodyParser = require('body-parser');
+
+
+
+
+app.listen(3001, () => {
+    console.log('console is listening on port 3001')
+});
