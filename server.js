f8d674c681de01bd2c6476235549ff74e39a120c
Ashish Singh <ashishcodes4@gmail.com>
2018-04-02 14:34:35 -0400

    initial commit

--- server.js
+++ server.js
@@ -2,8 +2,37 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const db = {
+    users:  [
+        {
+            id: 124,
+            name: "Ashish",
+            email: "ashishcodes4@gmail.com",
+            password: "cookies",
+            date: new Date()
+        },
+        {
+            id: 125,
+            name: "Jarvis",
+            email: "jarvis@gmail.com",
+            password: "bananas",
+            date: new Date()
+        }
+    ]
+}
 
+app.get('/', (req, res)=> {
+    res.json('this is working');
+})
 
+app.get('/signin', (req, res)=> {
+    if(req.body.email === db.users[0].email 
+        && req.body.password === db.users[0].password) {
+            res.json('logged in succesfuly');
+        } else {
+            res.status(400).json('error logging in');
+        }
+} )
 
 app.listen(3001, () => {
     console.log('console is listening on port 3001')
